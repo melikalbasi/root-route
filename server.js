1f89e604acfa738290fc1a7c5fe0a247c60a33f6
Melika Kalbasi <melikalbasi@gmail.com>
2019-02-11 19:27:52 -0800

    set up server

--- server.js
+++ server.js
@@ -1,24 +1,76 @@
 const express = require("express");
+var bodyParser = require("body-parser");
+var passport = require("passport");
 
-const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
+var db = require("./models");
 
 // Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-// Add routes, both API and view
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("client/build"));
+
 app.use(routes);
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/rootroute");
+app.use(passport.initialize());
+require("./config/passport");
 
-// Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+var user;
+
+
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
+);
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/", session: false }),
+  function(req, res) {
+      user = req.user;
+      db.User.create(user)
+        .then(data => console.log("this is your data: ", data))
+        .catch(err => console.log(err))
+      console.log(req.user)
+      var token = req.user.token;
+      console.log(token)
+      res.redirect("http://localhost:3000/paths");
+      // res.redirect("http://localhost:3000?token=" + token);
+  }
+);
+
+app.get('/logout', function(req, res) {
+  console.log("logged out!");
+  console.log("are you there? ", user)
+  user = null;
+  req.logout();
+  res.redirect('/');
+});
+
+
+// Checks if a user is logged in
+// const accessProtectionMiddleware = (req, res, next) => { 
+//   console.log("RES AUTHENTICATED ", req.isAuthenticated()) 
+//   if (req.isAuthenticated()) {
+//     next();
+//   } else {
+//     res.status(403).json({
+//       message: 'must be logged in to continue',
+//     });
+//   }
+// };
+
+// A secret endpoint accessible only to logged-in users
+app.get('/protected', (req, res) => {  
+  res.json(user);
+});
+
+
+
+
+db.sequelize.sync({ force: false }).then(function() {
+  app.listen(PORT, function() {
+    console.log(`App listening on PORT ${PORT} -- http://localhost:${PORT}/`);
+  });
 });
