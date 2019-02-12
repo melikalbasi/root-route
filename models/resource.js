3dcad52855e29126368dd7f78ed0830a9631fd37
Melika Kalbasi <melikalbasi@gmail.com>
2019-02-11 19:53:40 -0800

    update resource model

--- models/resource.js
+++ models/resource.js
@@ -1,26 +1,39 @@
 module.exports = function (sequelize, DataTypes) {
-  const Resource = sequelize.define("Resource", {
-    name: {
-      type: Sequelize.STRING,
-      validate: {
-        len: [1, 50]
+  var Resource = sequelize.define("Resource", {
+      name: {
+          type: DataTypes.STRING,
+          validate: {
+              len: [1, 100]
+          }
+      },
+      description: {
+          type: DataTypes.STRING,
+          validate: {
+              len: [1, 1000]
+          }
+      },
+      image: {
+          type: DataTypes.STRING,
+          validate: {
+              len: [1, 500]
+          }
+      },
+      link: {
+          type: DataTypes.STRING,
+          validate: {
+              len: [1, 500]
+          }
       }
-    },
-    subjectId: {
-      
-    },
-    description: {
-      type: Sequelize.STRING,
-      validate: {
-        len: [2, 200]
-      }
-    },
-    image: {
-      type: Sequelize.STRING,
-      validate: {
-        notNull: true
-      }
-    }
+      // courses complete set to a boolean
   });
+
+  Resource.associate = function (models) {
+      Resource.belongsTo(models.Subject, {
+          foreignKey: {
+              allowNull: false
+          }
+      });
+  }
+
   return Resource;
-}
+};
