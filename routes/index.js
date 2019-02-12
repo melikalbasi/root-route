2d11ec57150f5f793319c888144b660c6da165d7
Melika Kalbasi <melikalbasi@gmail.com>
2019-02-11 19:51:14 -0800

    update routes

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,47 @@
 const path = require("path");
 const router = require("express").Router();
+const pathsController = require("../controllers/pathsController");
 
-// API Routes
-router.use("/api", apiRoutes);
+router.route("/api/paths")
+  .get(pathsController.findAll)
+
+router.route("/api/paths/:pathid")
+  .get(pathsController.findSubjects)
+
+router.route("/api/paths/:pathid/subjects/:subjectid")
+  .get(pathsController.findResources)
+
+router.route("/api/review/:resourceid/:reviewContent")
+  .post(pathsController.submitReview)
+
+router.route("/api/review/:resourceid")
+  .get(pathsController.getReviews)
+
+router.route("/api/logout").get(function () {
+  req.logout()
+  res.redirect('/');
+})
+
 
 // If no API routes are hit, send the React app
-router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../clients/build/index.html"));
-});
+// router.use(function(req, res) {
+//   res.sendFile(path.join(__dirname, "../client/public/index.html"));
+// });
 
 module.exports = router;
+
+
+
+
+// const path = require("path");
+// const router = require("express").Router();
+
+// // API Routes
+// router.use("/api", apiRoutes);
+
+// // If no API routes are hit, send the React app
+// router.use(function(req, res) {
+//   res.sendFile(path.join(__dirname, "../clients/build/index.html"));
+// });
+
+// module.exports = router;
