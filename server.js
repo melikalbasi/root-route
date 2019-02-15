53c5759ddebf4d2d6950b5b6447a88f8a5632391
Melika Kalbasi <melikalbasi@gmail.com>
2019-02-14 16:54:03 -0800

    jawsdb connection

--- server.js
+++ server.js
@@ -65,6 +65,28 @@ app.get('/protected', (req, res) => {
   res.json(user);
 });
 
+var mysql = require("mysql");
+if (process.env.JAWSDB_URL) {
+var connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "w3kka4ieusupj9rs"
+  });
+}
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+module.exports = connection;
+
+
 
 db.sequelize.sync({ force: false }).then(function() {
   app.listen(PORT, function() {
